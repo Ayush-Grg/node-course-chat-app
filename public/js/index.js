cb246a96bf22479a6b24e396790322a6dbe0af06
Ayush-Grg <gurungayushman@gmail.com>
2018-03-29 14:14:21 +0530

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,27 @@
+var socket = io();
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+ 
+  // socket.emit('createEmail', {
+  //   to: 'jane@example.com',
+  //   text: 'Hey. This is John.',
+  // });
+
+  socket.emit('createMessage', {
+    from: 'John',
+    text: 'Yup, that works for me.'
+  });
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+// socket.on('newEmail', function (email) {
+//   console.log('New email', email);
+// });
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+});
